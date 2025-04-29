ffeb5303ceec8f08f82cc330bb80f6354c1481da
taeung515 <xodndd515@gmail.com>
2025-04-29 18:33:32 +0900

    필수 Lv3 구현

--- src/com/example/kiosk/Kiosk.java
+++ src/com/example/kiosk/Kiosk.java
@@ -1,5 +1,51 @@
 package com.example.kiosk;
 
+import java.util.List;
+import java.util.Scanner;
+
 public class Kiosk {
+    private final List<MenuItem> menuItems;
+
+    public Kiosk(List<MenuItem> menuItems) {
+        this.menuItems = menuItems;
+    }
+
+    //메인부에서 사용할 메서드 public
+    public void start() {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            printMenuItems();
+            int inputNum = scanner.nextInt();
+            if (inputNum == 0) {
+                System.out.println("프로그램을 종료합니다.");
+                return;
+            }
+            printSelectedMenuItem(inputNum);
+        }
+    }
+
+    //메뉴들을 출력하는 메서드
+    private void printMenuItems() {
+        System.out.println("\n[ SHAKESHACK MENU ]");
+        for (int i = 0; i < menuItems.size(); i++) {
+            MenuItem menuItem = menuItems.get(i);
+            System.out.printf("%d. %-12s | W %.1f | %s%n", i + 1, menuItem.getName(), menuItem.getPrice(), menuItem.getDescription());
+        }
+        System.out.println("0. 종료\t\t\t| 종료");
+        System.out.print("선택(번호를 입력하세요.) ");
+    }
+
+    //선택한 메뉴를 보여주는 메서드
+    private void printSelectedMenuItem(int inputNum) {
+        if (1 <= inputNum && inputNum <= menuItems.size()) {
+            MenuItem selected = menuItems.get(inputNum - 1);
+            System.out.println("\n=== 선택하신 메뉴 ===");
+            System.out.println("메뉴명 : " + selected.getName());
+            System.out.printf("가격   : W %.1f%n", selected.getPrice());
+            System.out.println("설명   : " + selected.getDescription());
+        } else {
+            System.out.println("잘못된 번호입니다. 메뉴 확인 바랍니다.");
+        }
+    }
 
 }
--- src/com/example/kiosk/Main.java
+++ src/com/example/kiosk/Main.java
@@ -2,11 +2,16 @@ package com.example.kiosk;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
+        Kiosk kiosk = new Kiosk(createMenuItem());
+        kiosk.start();
+    }
+
+    //메뉴 추가를 위한 메서드
+    private static List<MenuItem> createMenuItem() {
         List<MenuItem> menuItems = new ArrayList<MenuItem>();
 
         menuItems.add(new MenuItem("ShackBurger", 6.9, "토마토, 양상추, 쉑소스가 토핑된 치즈버거"));
@@ -14,32 +19,6 @@ public class Main {
         menuItems.add(new MenuItem("Cheeseburger", 6.9, "포테이토 번과 비프패티, 치즈가 토핑된 치즈버거"));
         menuItems.add(new MenuItem("Hamburger", 5.4, "비프패티를 기반으로 야채가 들어간 기본버거"));
 
-        Scanner scanner = new Scanner(System.in);
-        while (true) {
-            for (int i = 0; i < menuItems.size(); i++) {
-                MenuItem menuItem = menuItems.get(i);
-                System.out.printf("%d. %-12s | W %.1f | %s%n", i + 1, menuItem.getName(), menuItem.getPrice(), menuItem.getDescription());
-            }
-            System.out.println("0. 종료\t\t\t| 종료");
-            System.out.print("선택(번호를 입력하세요.) ");
-
-            int inputNum = scanner.nextInt();
-
-            if (inputNum == 0) {
-                System.out.println("프로그램을 종료합니다.");
-                return;
-            } else if (1 <= inputNum && inputNum <= menuItems.size()) {
-                MenuItem selected = menuItems.get(inputNum - 1);
-                System.out.println("\n=== 선택하신 메뉴 ===");
-                System.out.println("메뉴명 : " + selected.getName());
-                System.out.printf("가격   : W %.1f%n", selected.getPrice());
-                System.out.println("설명   : " + selected.getDescription());
-            } else {
-                System.out.println("잘못된 번호입니다. 메뉴 확인 바랍니다.");
-            }
-            //메뉴 추가되어도 반복문 수정 없게 하기 위하여 switch > if변경
-
-        }
-
+        return menuItems;
     }
 }
